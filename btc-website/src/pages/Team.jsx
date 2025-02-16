0a725cd1ed7ede302caf1faf60a4683b824dc9c0
harshvaghanii <harshvaghani98@gmail.com>
2025-02-16 03:35:48 -0600

    CMS Working for Team Members, Added title and favicon from the previous website

--- btc-website/src/pages/Team.jsx
+++ btc-website/src/pages/Team.jsx
@@ -30,11 +30,11 @@ export const Team = () => {
           id: doc.id,
           ...doc.data(),
         }));
-
+        console.log(teamMembers);
         // Filter team members based on roles
         const eboardMembers = teamMembers
-          .filter((member) => member.isExecutiveBoard)
-          .sort((a, b) => a.priority - b.priority || a.firstfirstName.localeCompare(b.firstfirstName));
+          .filter((member) => member.isEboardMember)
+          .sort((a, b) => a.priority - b.priority || a.firstName.localeCompare(b.firstName));
 
         const pmMembers = teamMembers
           .filter((member) => member.isProductManager)
@@ -45,7 +45,7 @@ export const Team = () => {
           .sort((a, b) => a.priority - b.priority || a.firstName.localeCompare(b.firstName));
 
         const uiuxMembers = teamMembers
-          .filter((member) => member.isUiUx)
+          .filter((member) => member.isUIDesigner)
           .sort((a, b) => a.priority - b.priority || a.firstName.localeCompare(b.firstName));
 
         setEboard(eboardMembers);
@@ -67,7 +67,7 @@ export const Team = () => {
   return (
     <div>
       <section
-        classfirstName="min-h-screen w-full flex flex-col md:flex-row justify-center items-center gap-8 md:px-20 px-10 bg-[#F5F5F5] bg-[url(/btc_background.png)] bg-no-repeat bg-[length:30%_auto] bg-[left_bottom_5rem] bg-left py:10 md:py-20"
+        className="min-h-screen w-full flex flex-col md:flex-row justify-center items-center gap-8 md:px-20 px-10 bg-[#F5F5F5] bg-[url(/btc_background.png)] bg-no-repeat bg-[length:30%_auto] bg-[left_bottom_5rem] bg-left py:10 md:py-20"
         id="about"
       >
         <motion.div
@@ -75,19 +75,19 @@ export const Team = () => {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.6, delay: 0.2 }}
-          classfirstName="flex flex-col gap-6 md:w-1/2 w-full md:order-1 order-2"
+          className="flex flex-col gap-6 md:w-1/2 w-full md:order-1 order-2"
         >
-          <h2 classfirstName="text-[#212529] md:text-left text-center md:text-5xl text-4xl font-bold font-inter mt-10 md:mt-0">
+          <h2 className="text-[#212529] md:text-left text-center md:text-5xl text-4xl font-bold font-inter mt-10 md:mt-0">
             Meet the BTC Team <br />
           </h2>
-          <p classfirstName="text-[#212529] md:text-left text-center md:text-lg text-base font-normal font-inter max-w-[600px]">
+          <p className="text-[#212529] md:text-left text-center md:text-lg text-base font-normal font-inter max-w-[600px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat.
           </p>
-          <button classfirstName="bg-btc mb-10 md:mb-0 hover:bg-btcdark text-white py-4 px-4 rounded-lg transition-colors items-center justify-center gap-2 md:w-1/4 w-1/2 md:mx-0 mx-auto font-bold">
-            Join Now <i classfirstName="fa-solid fa-arrow-right text-sm"></i>
+          <button className="bg-btc mb-10 md:mb-0 hover:bg-btcdark text-white py-4 px-4 rounded-lg transition-colors items-center justify-center gap-2 md:w-1/4 w-1/2 md:mx-0 mx-auto font-bold">
+            Join Now <i className="fa-solid fa-arrow-right text-sm"></i>
           </button>
         </motion.div>
         <motion.div
@@ -95,7 +95,7 @@ export const Team = () => {
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           transition={{ duration: 0.6, delay: 0.4 }}
-          classfirstName="md:w-[735px] md:h-[547px] bg-[#d9d9d9] w-[300px] h-[250px] md:order-2 order-1 mt-10 md:mt-0"
+          className="md:w-[735px] md:h-[547px] bg-[#d9d9d9] w-[300px] h-[250px] md:order-2 order-1 mt-10 md:mt-0"
         />
       </section>
       <TeamMember title={"Executive Board"} position={eboard} />
